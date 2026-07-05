b1b2126767638c7e73dee0729a759c13820f8f16
kazrkb <kazrkb@gmail.com>
2026-07-05 16:09:52 +0600

    lecture 1 completed

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-from 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, JSONResponse
+
+app = FastAPI()
+
+
+posts = [
+    {"id": 1, "title": "First Post", "content": "This is the first post."},
+    {"id": 2, "title": "Second Post", "content": "This is the second post."},
+    {"id": 3, "title": "Third Post", "content": "This is the third post."}
+]
+
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
+def home():
+    return f"<h1>{posts[0]['title']}</h1> <p>Use the /api/posts endpoint to get all posts.</p>"
+    # return posts
+
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
